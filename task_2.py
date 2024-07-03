13bf1e25c408e8fbb88e1886b3bb8f85e4535f0f
bogdankopytko <bo.kopytko@gmail.com>
2024-07-03 10:05:01 +0200

    Added validation for the entry parameters in function.

--- task_2.py
+++ task_2.py
@@ -3,10 +3,13 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     all_tickets = []
-    for i in range (min, max + 1, 1):
-        all_tickets.append(i)
-    tickets = random.sample(all_tickets, quantity)
-    return tickets
+    if min >= 1 and max <= 1000 and quantity in range (min, max + 1):
+        for i in range (min, max + 1, 1):
+            all_tickets.append(i)
+        tickets = random.sample(all_tickets, quantity)
+        return tickets
+    else:
+        return all_tickets
 
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
