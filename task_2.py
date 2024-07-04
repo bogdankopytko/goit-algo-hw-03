d992a23849d5713fcb1c4fcfaee396d92a0df858
bogdankopytko <bo.kopytko@gmail.com>
2024-07-04 09:43:09 +0200

    Changed if/else statement.

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     all_tickets = []
-    if min >= 1 and max <= 1000 and quantity in range (min, max + 1):
+    if min >= 1 and max <= 1000 and quantity <= (max - min):
         for i in range (min, max + 1, 1):
             all_tickets.append(i)
         tickets = random.sample(all_tickets, quantity)
@@ -12,5 +12,5 @@ def get_numbers_ticket(min, max, quantity):
         return all_tickets
 
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 20, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
