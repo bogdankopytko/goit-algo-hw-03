b15a52676f2a7c56409bb32c3e6a05174aeeba7f
bogdankopytko <bo.kopytko@gmail.com>
2024-07-04 13:40:40 +0200

    Added sorting method to the function output.

--- task_2.py
+++ task_2.py
@@ -4,13 +4,13 @@ import random
 def get_numbers_ticket(min, max, quantity):
     all_tickets = []
     if min >= 1 and max <= 1000 and quantity <= (max - min):
-        for i in range (min, max + 1, 1):
+        for i in range(min, max + 1, 1):
             all_tickets.append(i)
         tickets = random.sample(all_tickets, quantity)
-        return tickets
+        return sorted(tickets)
     else:
         return all_tickets
 
 
-lottery_numbers = get_numbers_ticket(10, 20, 5)
-print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print(f'Ваші лотерейні числа: {lottery_numbers}')
